bda26ed15a3d9edd2becc1ceca4aecd616b0c9bd
mukoko <mukokogarciagx@gmail.com>
2025-03-08 17:18:55 +0100

    liste des utilisateurs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , request , session , redirect, render_template
+from flask import Flask , request , session , redirect, render_template , flash
 import sqlite3
 
 
@@ -18,8 +18,63 @@ def home():
 #
 @app.route("/login", methods = ['POST','GET'])
 def login():
+    if request.method == 'POST':
+
+        user = request.form['username']
+        pwd  = request.form['pwd']
+
+        with sqlite3.connect("aia.db") as con :
+            cur = con.cursor()
+            cur.execute("select * from acteurs where nomUtilisateur = ? and passwordActeur = ? ",[user,pwd]) 
+            data  = cur.fetchone()
+
+            if data :
+                session['aia'] = True
+                session['id']  = data[0]
+                session['noms'] = data[1]
+                session['email'] = data[2]
+                session['user']  = data[3]
+                session['role']  = data[4]
+
+                return redirect('/admin')
+
+            else:
+                flash("mot de passe incorrecte")  
+                  
+            
+
     return render_template('back/auth-login.html')
 
+#
+#
+# admin
+@app.route("/admin")
+def admin():
+    if 'aia' in session:
+        return render_template('back/index.html') 
+    else:
+        return redirect('/login')
+##
+#
+#  deconnection 
+@app.route("/deco")
+def deco():
+    session.clear()
+    return redirect('/')
+
+#
+# table-datatable.html
+@app.route('/listeUser') 
+def listeUser():
+    if 'aia' in session:
+        with sqlite3.connect('aia.db') as con :
+            cur = con.cursor()
+            cur.execute("select * from acteurs")
+            data = cur.fetchall()
+
+            return render_template('back/table-datatable.html',data = data) 
+    else:
+        return redirect('/login')
 ## boucle 
 
 if __name__ == '__main__':

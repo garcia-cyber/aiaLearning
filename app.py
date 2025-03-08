13c8a73a420306ae28c352f7ca9ead0c7fd90c9d
mukoko <mukokogarciagx@gmail.com>
2025-03-08 18:45:28 +0100

    ajout de la possibilite de cree un compte apprenant

--- app.py
+++ app.py
@@ -75,7 +75,55 @@ def listeUser():
             return render_template('back/table-datatable.html',data = data) 
     else:
         return redirect('/login')
+    
+
+###
+# ###
+# creation de compte
+# 
+# 
+@app.route("/apprenant", methods = ['POST','GET'])
+def apprenant():
+    if request.method == 'POST':
+        noms = request.form['noms'] 
+        email = request.form['email']
+        user = request.form['user']
+        pwd  = request.form['pwd']
+        pwd2 = request.form['pwd2']
+        role = 'apprenant'
+
+        with sqlite3.connect("aia.db") as con :
+            # verification du email 
+            mail = con.cursor()
+            mail.execute("select * from acteurs where emailActeur = ? ",[email]) 
+            dataMail = mail.fetchone()
+
+            # verification du nom utilisateur 
+            use = con.cursor()
+            use.execute("select * from acteurs where nomUtilisateur = ?", [user]) 
+            dataUse = use.fetchone() 
+
+            if dataMail:
+                flash('le email existe deja ')
+            elif dataUse:
+                flash("le nom d'utilisateur existe deja") 
+            elif pwd2 == pwd:
+                add = con.cursor()
+                add.execute("insert into acteurs(nomsActeur,emailActeur,nomUtilisateur,fonctionActeur,passwordActeur) values(?,?,?,?,?)",[noms,email,user,role,pwd])
+                con.commit()
+                
+                return redirect('/login')
+            else:
+                flash("le mot de passe doit etre conforme")        
+
+
+        
+
+    return render_template('back/auth-register.html')
+
+
 ## boucle 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
